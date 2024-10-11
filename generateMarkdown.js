3aa69412df19d606f38fc6e137a02fdd9297caad
RiverStephenson <riverstephenson06@outlook.com>
2024-10-11 08:31:14 -0500

    added license section and links

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -14,11 +14,31 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "Apache") {
+    return "https://opensource.org/licenses/Apache-2.0"
+  } else if (license === "Boost") {
+    return "https://www.boost.org/LICENSE_1_0.txt"
+  } else if (license === "Eclipse") {
+    return "https://opensource.org/licenses/EPL-1.0"
+  } else if (license === undefined) {
+    return ""
+  };
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === "Apache") {
+    return "The license used for this project was Apache";
+  } else if (license === "Boost") {
+    return "The license used for this project was Boost";
+  } else if (license === "Eclipse") {
+    return "The license used for this project was Eclipse";
+  } else if (license === undefined) {
+    return "";
+  }
+};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
--- index.js
+++ index.js
@@ -1,9 +1,8 @@
+// TODO: Include packages needed for this application
 import fs from 'fs';
 import inquirer from 'inquirer';
 import generateMarkdown from './generateMarkdown.js';
 
-
-// TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
 const questions = [
